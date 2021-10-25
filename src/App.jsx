cad7290c6df13df74891125d12f9015e3abc7414
Mohammad Bagheri <m.bagheri@naringames.com>
2021-10-25 09:14:57 +0330

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+//import css reset file 
+import "./assets/css/css-reset.css"
+
+function App() {
+  return (
+    <React.Fragment>
+    <h1>hi</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
